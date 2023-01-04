bebbf13cdff94ba92c74b8f07ef373899212bc7d
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2023-01-04 13:56:15 -0600

    failed attempt at adding tailwindcss

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 var static = require('node-static');
 
-var file = new(static.Server)("index.html");
+var file = new(static.Server)("./dist/index.html");
 app.get("/", (req, res) => {
-    file.serve(req, res);
+    file.serve(req, res); 
 })
 
 app.listen(5000, () => { console.log("Server started on port 5000") })
