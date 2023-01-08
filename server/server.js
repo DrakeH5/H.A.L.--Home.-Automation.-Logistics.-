a488a44f89ea472b8f0a5d2ae123bd05f77f1bcb
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2023-01-07 18:47:31 -0600

    backend for alarm clock

--- server/server.js
+++ server/server.js
@@ -5,5 +5,19 @@ const app = express();
 app.get("/", (req, res) => {
     res.json("Hello World")
 })
-
+ 
 app.listen(1001, () => { console.log("Server started on port 1001") })
+
+
+var wakeUpAlarmTimes = [[7,00],[7,00],[7,00],[7,00],[7,00],[7,00],[10,00],[10,00]]
+setInterval(() => {
+    var date = new Date();
+    let day = date.getDay()
+    if(date.getHours() == wakeUpAlarmTimes[day][0] && date.getMinutes() == wakeUpAlarmTimes[day][1]) {
+        playWakeUpAlarm()
+    }
+}, 60000);
+async function playWakeUpAlarm() {
+    let {playAudioFile} = await import('audic');
+    await playAudioFile('./audio files/let-the-day-begin--the-call.wav');
+  }
